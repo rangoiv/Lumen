d0ff1a22bad6dcea50320f71d20d25de60c17912
rangoiv <40663576+rangoiv@users.noreply.github.com>
2023-04-26 13:07:28 +0200

    Predict for multi files, cleanup (#2)

--- mp3_2_wav.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-from pathlib import Path
-
-for path, _, files in os.walk(str(noise_path)):
-     for i, file in enumerate(files):
-             if file.endswith('.mp3'):
-                     subprocess.call(['ffmpeg', '-i', noise_path/file, str(noise_path/f"{i}.wav")])
